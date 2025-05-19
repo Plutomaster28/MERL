2ec371cc4ef847b0027c904d269367123444843f
Miyamii <allthebadnameswheretaken@gmail.com>
2025-05-19 08:40:40 -0500

    added preperations for tetra integration

--- root/config.h
+++ root/config.h
@@ -4,11 +4,12 @@
 // System configuration constants
 #define FIRMWARE_VERSION "0.0.4"
 #define TOTAL_MEMORY_MB 500
-#define BIOS "Droplet" // Uses experimental Meisei GOLDFISH BIOS
+#define BIOS "Droplet"
 
 // Additional compile-time configuration
 #define OS_VERSION "MERL-V v0.1"
 #define ENABLE_KAIROS 1
 #define MAX_USERS 8
+#define TETRA_REPO_LOCATION "./tetra_repos"
 
 #endif // CONFIG_H
