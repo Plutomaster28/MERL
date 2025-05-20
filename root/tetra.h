ee0e5bbc593e00ecf3c5ea45c63de23217d8b8c2
Miyamii <allthebadnameswheretaken@gmail.com>
2025-05-20 08:33:45 -0500

    fully implemented tetra

--- root/tetra.h
+++ root/tetra.h
@@ -5,9 +5,9 @@ void tetra_command(int argc, char **argv);
 
 // Subcommands
 void tetra_download(int argc, char **argv);
-void tetra_update(int argc, char **argv);
 void tetra_remove(int argc, char **argv);
 void tetra_move(int argc, char **argv);
 void tetra_set_location(int argc, char **argv);
+void tetra_list(int argc, char **argv);
 
 #endif // TETRA_H
